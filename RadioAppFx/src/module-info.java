f01a87cd9256e84f067bd3ef8655a0ec0db9dcc5
Ferdia <48727715+FerdiaMT@users.noreply.github.com>
2024-11-01 17:04:44 +0000

    Delete RadioAppFx directory

--- RadioAppFx/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module RadioAppFx {
-	requires javafx.controls;
-	requires java.desktop;
-	requires javafx.media;
-	requires javafx.graphics;
-	requires javafx.base;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
